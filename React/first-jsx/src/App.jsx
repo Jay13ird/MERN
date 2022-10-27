017f206f75d8c225d41429a8429e45359ace7613
Jay13ird <JayBird2025@Gmail.com>
2022-10-27 00:28:31 -0400

    first jsx

---
+++ React/first-jsx/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h3>Things I need to do:</h3>
+        <ul>
+          <li>Learn react</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
