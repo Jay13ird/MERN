5aea53485d072c812e3df8089f539d384930438c
Jay13ird <JayBird2025@Gmail.com>
2022-10-28 14:38:52 -0400

    Prop it up

---
+++ React/prop-it-up/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PersonCard from './components/PersonCard'
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName="Rick" lastName="Sanchez" age="70" hairColor="Light Blue"/>
+      <PersonCard firstName="Morty" lastName="Smith" age="15" hairColor="Brown"/>
+      <PersonCard firstName="Summer" lastName="Smith" age="17" hairColor="Red"/>
+      <PersonCard firstName="Beth" lastName="Smith" age="34" hairColor="Blonde"/>
+    </div>
+  );
+}
+
+export default App;
