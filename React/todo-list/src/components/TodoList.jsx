f1d994536d12ede43c8a9d74cc9c6d9be5c6e069
Jay13ird <JayBird2025@Gmail.com>
2022-11-02 18:28:13 -0400

    Todo List

--- React/todo-list/src/components/TodoList.jsx
+++ React/todo-list/src/components/TodoList.jsx
@@ -7,7 +7,7 @@ const TodoList = (props) => {
         <div>
             {props.todoList.map((item, i) =>
                 <div key={i} className="showList">
-                    <p className={item.complete === true ? `completed` : `notCompleted`}>{item.text}</p>
+                    <p className={item.complete === true ? "completed" : "notCompleted"}>{item.text}</p>
                     <input type="checkbox" checked={item.complete} onChange={() => props.changeItem(i)}/>
                     <button onClick={() => props.removeItem(i)}>Delete</button>
                 </div>
